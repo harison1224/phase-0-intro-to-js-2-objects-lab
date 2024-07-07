2aa24f2d1620084d7c3886b70ff129af3e1ab84a
harison1224 <harisonochieng0@gmail.com>
2024-07-07 23:17:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Harison Ochieng",
+    streetAddress: "Nairobi,00100",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const employeeCopy = {...employee};
+    employeeCopy[key] = value;
+    return employeeCopy;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const employeeCopy = {...employee};
+    delete employeeCopy[key];
+    return employeeCopy;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
